e9815638e5a69e96134f52daf882ae8ab4a61dcb
anton <zanarovanthon@yandex.com>
2024-02-20 07:16:30 +0300

    commit4

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ const cardTemplate = document.querySelector('#card-template').content;
 const content = document.querySelector('.places__list');
 
 
-function createCard(item) {
+function createCard(item, deliteFunc) {
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
     cardElement.querySelector('.card__image').src = item.link;
@@ -11,13 +11,13 @@ function createCard(item) {
     cardElement.querySelector('.card__title').textContent = item.name;
 
     const deliteButton = cardElement.querySelector('.card__delete-button');
-    deliteButton.addEventListener('click', deliteCard);
+    deliteButton.addEventListener('click', deliteFunc);
 
     return cardElement;
 };
 
 initialCards.forEach( function (item) { 
-   content.append(createCard(item));
+   content.append(createCard(item, deliteCard));
 });
 
 function deliteCard(evt) {
